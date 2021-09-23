87394e12ee38e2f47d18fa9e57814a09f07ddaa0
Thais Souza <p1200026@brasilprev.com.br>
2021-09-23 18:56:49 -0300

    menu dinamico + troca de perfil

--- app/src/app/components/master-layout/master-layout.component.ts
+++ app/src/app/components/master-layout/master-layout.component.ts
@@ -1,13 +1,65 @@
 import { Component, OnDestroy, OnInit, ChangeDetectorRef } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-master-layout',
   templateUrl: './master-layout.component.html',
   styleUrls: ['./master-layout.component.scss']
 })
-export class MasterLayoutComponent  {
-    
+export class MasterLayoutComponent implements OnInit {
+  doctorMenu = [
+    {
+      name: 'Dashboard',
+      path: '/doctor/dashboard'
+    },
+    {
+      name: 'Agenda',
+      path: '/doctor/calendar'
+    },
+    {
+      name: 'Pacientes',
+      path: '/doctor/pacient-history'
+    }
+  ];
+
+  medicalClinicMenu = [
+    {
+      name: 'Dashboard',
+      path: '/medical-clinic/dashboard'
+    },
+    {
+      name: 'Agenda',
+      path: '/medical-clinic/calendar'
+    },
+    {
+      name: 'Pacientes',
+      path: '/medical-clinic/pacient-history'
+    }
+  ];
+
+  pacientMenu = [
+    {
+      name: 'Dashboard',
+      path: '/pacient/dashboard'
+    },
+    {
+      name: 'Médicos',
+      path: '/pacient/doctor-calendar'
+    },
+    {
+      name: 'Clínicas',
+      path: '/pacient/medical-clinic-list'
+    },
+    {
+      name: 'Histórico',
+      path: '/pacient/history'
+    },
+  ];
+
+  href: string;
+  profileType: string;
+
   mobileQuery: MediaQueryList;
 
   fillerNav = Array.from({ length: 2 }, (_, i) => `Nav Item ${i + 1}`);
@@ -17,10 +69,27 @@ export class MasterLayoutComponent  {
 
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private router: Router
+  ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnInit() {
+    this.href = this.router.url;
+    
+    if(this.href.includes('doctor')) {
+      this.profileType = 'doctor';
+    } else if(this.href.includes('medical-clinic')) {
+      this.profileType = 'medical-clinic';
+    }else if(this.href.includes('pacient')) {
+      this.profileType = 'pacient';
+    }
+
   }
 
   ngOnDestroy(): void {
--- app/src/app/pages/pacient/pacient-routing.module.ts
+++ app/src/app/pages/pacient/pacient-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from '../pacient/dashboard/dashboard.component';
+import { DoctorCalendarComponent } from './doctor-calendar/doctor-calendar.component';
+import { MedicalClinicListComponent } from './medical-clinic-list/medical-clinic-list.component';
+import { HistoryComponent } from './history/history.component';
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'doctor-calendar',
+    component: DoctorCalendarComponent
+  },
+  {
+    path: 'medical-clinic-list',
+    component: MedicalClinicListComponent
+  },
+  {
+    path: 'history',
+    component: HistoryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
