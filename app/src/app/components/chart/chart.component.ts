7de09ab952cb3c3c87f767ce720cf1d0cd524d32
Thais Souza <p1200026@brasilprev.com.br>
2021-10-24 21:15:18 -0300

    dashboards ok

--- app/src/app/components/chart/chart.component.ts
+++ app/src/app/components/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 
 import {
   ChartComponent,
@@ -23,8 +23,10 @@ export type ChartOptions = {
   styleUrls: ['./chart.component.scss']
 })
 export class ChartsComponent {
+  @Input() viewType: string;
   @ViewChild("chart") chart: ChartComponent;
   public chartOptions: ChartOptions;
+  public chartOptionsClinic: ChartOptions;
 
   constructor() {
     this.chartOptions = {
@@ -35,6 +37,28 @@ export class ChartsComponent {
         },
         {
           name: "Retorno",
+          data: [11, 32, 45, 32, 34, 52, 41, 60, 93] //30, 20, 87
+        }
+      ],
+      chart: {
+        type: "area"
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "smooth"
+      }
+    };
+
+    this.chartOptionsClinic = {
+      series: [
+        {
+          name: "Exames realizados",
+          data: [31, 40, 28, 51, 42, 109, 100, 80, 67] //78, 28, 92
+        },
+        {
+          name: "Exames encaminhados",
           data: [11, 32, 45, 32, 34, 52, 41, 60, 93] //30, 20, 87
         }
       ],
